3df7eb9c9262845b36199d2824f5713526303c1a
Michael Vernick <michael.vernick@vonage.com>
2025-02-10 13:15:41 -0500

    Removed shell scripts from repo but kept locally

--- set_secrets.sh
+++
@@ -1,9 +0,0 @@
-vcr secret remove --name VONAAG_APP_ID
-vcr secret create --name VONAGE_APP_ID --value 183dcd42-8703-4782-9ec3-c03ed36f222c
-
-vcr secret remove --name VONAGE_PRIVATE_KEY
-vcr secret create --name VONAGE_PRIVATE_KEY --filename /Users/mvernick/Keys/vcr-starter.key
-
-vcr secret remove --name VIDEO_SERVICE_PROVIDER
-vcr secret create --name VIDEO_SERVICE_PROVIDER --value vonage
-
